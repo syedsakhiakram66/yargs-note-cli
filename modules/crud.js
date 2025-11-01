b7a69b8e59dabc5733288ee5a7992af58c75a6f4
Sakhi Akram <226042051+syedsakhiakram66@users.noreply.github.com>
2025-11-01 09:08:30 +0500

    create note operation created

---
+++ modules/crud.js
@@ -0,0 +1,11 @@
+import {insert, getDB, saveDB} from './db.js'
+
+export const newNote = async (note, tags) => {
+    const data = {
+        tags, 
+        content: note,
+        id: Date.now(),
+    }
+    await insert(data)
+    return data
+}
